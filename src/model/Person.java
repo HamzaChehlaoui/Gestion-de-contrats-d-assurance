1986df6ddb351e61f9861adc1de7521ed63fed72
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-09-24 10:59:29 +0100

    terminer le model Person

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,29 +1,47 @@
 package model;
 
-public abstract class Person{
+public abstract class Person {
 
-    private String nom ;
-    private String prenom ;
-    private String email ;
+    private String nom;
+    private String prenom;
+    private String email;
 
-    public void setNom(String newNom){
-        this.nom = newNom;
+    public Person(String nom, String prenom, String email) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
     }
-    public String getNom(){
+
+    protected Person() {
+    }
+
+    // Getters & Setters
+    public String getNom() {
         return nom;
     }
-    public void setPrenom(String newPrenom){
-        this.prenom = newPrenom;
+
+    public void setNom(String newNom) {
+        this.nom = newNom;
     }
-    public String getPrenom(){
+
+    public String getPrenom() {
         return prenom;
     }
-    public void setEmail(String newEmail){
-        this.email = newEmail;
+
+    public void setPrenom(String newPrenom) {
+        this.prenom = newPrenom;
     }
-    public String getEmail(){
+
+    public String getEmail() {
         return email;
     }
 
+    public void setEmail(String newEmail) {
+        this.email = newEmail;
+    }
 
+    @Override
+    public String toString() {
+        return nom + " " + prenom + " (" + email + ")";
+    }
 }
