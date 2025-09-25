69986c26f189b2cce39c3c8c6e22e9f886cdd4e6
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-09-25 16:58:04 +0100

    creat le menu prinsipale

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,64 @@
 
-import java.util.HashMap;
-public class Main{
+import java.sql.SQLException;
+import java.util.Scanner;
+import view.ClientView;
+import view.ContratView;
+import view.SinistreView;
 
+public class Main {
 
-    static public void main(String args[]){
+    private ClientView clientView;
+    private ContratView contratView;
+    private SinistreView sinistreView;
+    private Scanner scanner;
 
-        System.out.println("hello world");
+    public Main() throws SQLException {
+        this.clientView = new ClientView();
+        this.contratView = new ContratView();
+        this.sinistreView = new SinistreView();
+        this.scanner = new Scanner(System.in);
+    }
 
+    public void menuPrincipal() throws SQLException {
+        int choix;
+        do {
+            System.out.println("\n=== MENU PRINCIPAL ===");
+            System.out.println("1. Gestion des Clients");
+            System.out.println("2. Gestion des Contrats");
+            System.out.println("3. Gestion des Sinistres");
+            System.out.println("0. Quitter");
+            System.out.print("Choix: ");
+            choix = scanner.nextInt();
+            scanner.nextLine(); // Consommer le retour à la ligne
 
+            switch (choix) {
+                case 1:
+                    clientView.menu();
+                    break;
+                case 2:
+                    contratView.menu();
+                    break;
+                case 3:
+                    sinistreView.menu();
+                    break;
+                case 0:
+                    System.out.println("Au revoir !");
+                    break;
+                default:
+                    System.out.println("Choix invalide, réessayez !");
+                    break;
+            }
+
+        } while (choix != 0);
+    }
+
+    public static void main(String[] args) {
+        try {
+            Main mainView = new Main();
+            mainView.menuPrincipal();
+        } catch (SQLException e) {
+            System.err.println("Erreur lors de l'initialisation du système !");
+            e.printStackTrace();
+        }
     }
 }
