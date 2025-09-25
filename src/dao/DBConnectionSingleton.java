3a118f3daaf27c6eb4a07fc21c74a89b0eaed179
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-09-25 16:48:42 +0100

    create class coniction de basse donnée

--- src/dao/DBConnectionSingleton.java
+++ src/dao/DBConnectionSingleton.java
@@ -1,4 +1,30 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnectionSingleton {
+
+    private static DBConnectionSingleton instance; // instance unique
+    private Connection connection;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/assurance";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    private DBConnectionSingleton() throws SQLException {
+        this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static DBConnectionSingleton getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DBConnectionSingleton();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
